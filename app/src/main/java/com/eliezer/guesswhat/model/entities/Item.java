63bf7baccb738966bdbb78ffcbcfac750fbbd726
eliezergensburger <eliezer.jct@gmail.com>
2019-12-10 15:34:27 +0200

    Initial commit

---
+++ app/src/main/java/com/eliezer/guesswhat/model/entities/Item.java
@@ -0,0 +1,34 @@
+package com.eliezer.guesswhat.model.entities;
+
+public class Item {
+    private String Owner;
+    private String Status;
+    private  String Type;
+
+    public String getOwner() {
+        return Owner;
+    }
+
+    public void setOwner(String owner) {
+        Owner = owner;
+    }
+
+    public String getStatus() {
+        return Status;
+    }
+
+    public void setStatus(String status) {
+        Status = status;
+    }
+
+    public String getType() {
+        return Type;
+    }
+
+    public void setType(String type) {
+        Type = type;
+    }
+
+
+
+}
